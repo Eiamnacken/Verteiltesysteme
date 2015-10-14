15a9dafed6e09af8b9ca6821b687b65e6ac2a5dd
Eiamnacken <Sven2q@hotmail.de>
2015-10-14 14:39:25 +0200

    Client senden
    Client sendet nun eine Nachricht an den Server und bekommt eine Antwort von diesem.

--- EchoClient/src/EchoClient.java
+++ EchoClient/src/EchoClient.java
@@ -5,7 +5,7 @@ import java.net.*;
  * Created by sven on 12.10.15.
  */
 public class EchoClient {
-    private static final int BUFSIZE = 508;
+    private static final int BUFSIZE = 1024;
 
     public static void main(String[] args) {
         if (args.length!=2){
@@ -16,13 +16,14 @@ public class EchoClient {
         String host = args[0];
         int port = Integer.parseInt(args[1]);
         try (DatagramSocket socket = new DatagramSocket()){
-
+            byte[] buffer = new byte[1];
             InetAddress address = InetAddress.getByName(host);
-            DatagramPacket packetOut = new DatagramPacket(new byte[1],1,address,port);
+            DatagramPacket packetOut = new DatagramPacket(buffer,buffer.length,address,port);
+            packetOut.setData("Hallo an server".getBytes());
             socket.send(packetOut);
             DatagramPacket packetIn = new DatagramPacket(new byte[BUFSIZE],BUFSIZE);
             socket.receive(packetIn);
-            String received = new String(packetIn.getData(),0,packetIn.getLength());
+            String received = new String(packetIn.getData());
             System.out.println(received);
         } catch (IOException e) {
             e.printStackTrace();
