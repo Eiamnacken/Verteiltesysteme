2b9d8ed5d48f2370667ae09fdc8ddabe3780fca3
Sven Marquardt <Sven2q@hotmail.de>
2015-10-27 17:02:11 +0100

    Zitate
    Gui für die Zitate app gebaut nur oberflächlich ohne schnickschnack.
    Projekte bennant.

---
+++ ZItateApp/src/sample/Controller.java
@@ -0,0 +1,27 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+
+import javax.swing.*;
+
+public class Controller {
+    public TextField searchBar;
+    public TextArea zitatText;
+
+    public void searchFieldPressed(KeyEvent event) {
+        if (event.getCode()== KeyCode.ENTER) search();
+    }
+
+    public void searchZitate(ActionEvent actionEvent) {
+        search();
+    }
+
+    private void search(){
+
+    }
+}
