0ac6b62ee8cbb199c55bda4e0c9ee0b691ae861c
Sven Marquardt <Sven2q@hotmail.de>
2015-11-17 18:24:44 +0100

    Alles in den Transceiver verlägt

--- Transceiver/src/main/java/Transceiver/Transceiver.java
+++ Transceiver/src/main/java/Transceiver/Transceiver.java
@@ -4,34 +4,25 @@ import Transceiver.Socket.Receiver;
 import Transceiver.Socket.Transmitter;
 import Transceiver.Socket.UDPSocket;
 
+import java.io.IOException;
 import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 /**
  * Created by eiamnacken on 05.11.15.
  */
 public class Transceiver implements Runnable{
 
-
-    /**
-     * Thread um daten zu schreiben auf die Konsole
-     */
-    private Thread write;
-
-    /**
-     * Thread um daten von der Konsole zu lesen
-     */
-    private  Thread read;
-
     /**
      * Soll der #read beendet werden
      */
-    private boolean readExecute;
+    private boolean readExecute=false;
 
     /**
      * Soll der #write beendet werden
      */
-    private boolean writeExecute;
+    private boolean writeExecute=false;
 
     /**
      * Um daten zu erhalten
@@ -51,8 +42,6 @@ public class Transceiver implements Runnable{
     public Transceiver(int port, String host) throws SocketException, UnknownHostException {
         this.receiver= new Receiver(new UDPSocket(port));
         this.transmitter = new Transmitter(new UDPSocket(port,host));
-        this.read = new Thread();
-        this.write = new Thread();
     }
 
     /**
@@ -61,12 +50,37 @@ public class Transceiver implements Runnable{
      */
     public Transceiver(int port) throws SocketException {
         this.receiver = new Receiver(new UDPSocket(port));
-        this.write = new Thread();
     }
 
     @Override
     public void run() {
-        //TODO Logik implementieren beide threads zum lesen um empfangen
-
+        Scanner in = new Scanner(System.in);
+        Thread read = new Thread(){
+            public void run(){
+                while (!readExecute){
+                    StringBuilder builder = new StringBuilder();
+                    while (in.hasNext()){
+                        builder.append(in.next());
+                    }
+                    try {
+                        transmitter.send(builder.toString());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+        Thread write= new Thread(){
+          public void run() {
+              while (!writeExecute){
+                  try {
+                      String message = receiver.receive();
+                      System.out.println(message);
+                  } catch (IOException e) {
+                      e.printStackTrace();
+                  }
+              }
+          }
+        };
     }
 }
