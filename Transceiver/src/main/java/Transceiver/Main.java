17c9b32b78b28d9f8913778b4bfb053a59236893
Sven Marquardt <Sven2q@hotmail.de>
2015-11-17 23:41:47 +0100

    main hinzugefügt

--- Transceiver/src/main/java/Transceiver/Main.java
+++ Transceiver/src/main/java/Transceiver/Main.java
@@ -1,8 +1,21 @@
 package Transceiver;
 
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
 /**
  * Created by sven on 17.11.15.
  */
 public class Main {
 
+    public static void main(String[] args){
+        try {
+            Transceiver transceiver = new Transceiver(5000,"localhost");
+            transceiver.run();
+        } catch (SocketException e) {
+            e.printStackTrace();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+    }
 }
