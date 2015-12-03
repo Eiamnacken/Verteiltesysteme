d72b9ea102e96ae11e71951c45937119c239bd1d
Sven Marquardt <Sven2q@hotmail.de>
2015-12-03 01:05:33 +0100

    Gui für den chat client erstellt

---
+++ TCPChat/src/main/java/com/TCPChat/Main.java
@@ -0,0 +1,25 @@
+package com.TCPChat;
+
+import javafx.application.Application;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        StackPane root = new StackPane(new Label("Hello JavaFX World!"));
+
+        Rectangle2D visualBounds = Screen.getPrimary().getVisualBounds();
+        Scene scene = new Scene(root, visualBounds.getWidth(), visualBounds.getHeight());
+
+        stage.getIcons().add(new Image(Main.class.getResourceAsStream("/icon.png")));
+        stage.setScene(scene);
+        stage.show();
+    }
+}
