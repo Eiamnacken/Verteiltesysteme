91352b23172af762f809cac6691ae41130f2ee4f
Sven Marquardt <Sven2q@hotmail.de>
2015-12-07 12:24:16 +0100

    GUI fertig
    GUI fertiggestellt muss noch individuell gestaltet werden

---
+++ TCPChat/src/main/java/com/TCPChat/ChatController.java
@@ -0,0 +1,25 @@
+package com.TCPChat;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import org.controlsfx.control.HiddenSidesPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by sven on 07.12.15.
+ */
+public class ChatController implements Initializable{
+    public HiddenSidesPane hiddenPane;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+
+    }
+
+    public void sendComment(ActionEvent actionEvent) {
+
+    }
+}
